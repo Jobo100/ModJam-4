dd77d2cd6aae88370a073412d5b3557ff9c4bae9
Joe <josephdb100@gmail.com>
2014-05-15 00:46:44 -0600

    probably should have learned how to make a slab before mod jam lol

--- java/com/jdb/base/block/BlockCharredWoodSlab.java
+++ java/com/jdb/base/block/BlockCharredWoodSlab.java
@@ -3,6 +3,7 @@ package com.jdb.base.block;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockSlab;
 import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
 
 public class BlockCharredWoodSlab extends BlockSlab
 {
@@ -10,14 +11,18 @@ public class BlockCharredWoodSlab extends BlockSlab
 	public BlockCharredWoodSlab(boolean p_i45410_1_, Material material)
 	{
 		super(p_i45410_1_, material);
-		this.
-		
+		this.setBlockTextureName("charred wood");
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setStepSound(soundTypeWood);
+		this.setBlockName("Charred Wood");
+		this.setHardness(2.0F);
+		this.setResistance(7.0F);
 	}
 
 	@Override
 	public String func_150002_b(int var1)
 	{
-		return null;
+		return super.getUnlocalizedName();
 	}
 
 }
