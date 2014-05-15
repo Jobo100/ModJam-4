7cfeb917d26231147dbfc9232612db2bebf50748
Joe <josephdb100@gmail.com>
2014-05-15 17:28:47 -0600

    taking break

--- java/com/jdb/base/block/BlockCharredWood.java
+++ java/com/jdb/base/block/BlockCharredWood.java
@@ -1,5 +1,7 @@
 package com.jdb.base.block;
 
+import java.util.Random;
+
 import com.jdb.base.item.ItemKnife;
 import com.jdb.base.item.ItemSoulJar;
 
@@ -25,7 +27,6 @@ public class BlockCharredWood extends Block {
 		this.setBlockName("charredWood");
 		this.setHardness(2.0F);
 		this.setResistance(7.0F);
-		this.setTickRandomly(true);
 	}
 	
 	@Override
@@ -43,7 +44,7 @@ public class BlockCharredWood extends Block {
     	}
         return false;
     }
-    
+	
     public TileEntity createTileEntity(World world, int metadata)
     {
     	tileEntity = new TileEntityCharredWood();
--- java/com/jdb/base/block/TileEntityCharredWood.java
+++ java/com/jdb/base/block/TileEntityCharredWood.java
@@ -6,17 +6,20 @@ import net.minecraft.tileentity.TileEntity;
 public class TileEntityCharredWood extends TileEntity
 {
 		private int entityId = 0;	
+		private int time = 0;
 		
 		public void writeToNBT(NBTTagCompound nbtCompound)
 	    {
 	        super.writeToNBT(nbtCompound);
 	        nbtCompound.setInteger("Entity Id", entityId);
+	        nbtCompound.setInteger("Time", time);
 	    }
 
 	    public void readFromNBT(NBTTagCompound nbtCompound)
 	    {
 	        super.readFromNBT(nbtCompound);
 	        entityId = nbtCompound.getInteger("Entity ID");
+	        time = nbtCompound.getInteger("Time");
 	    }
 	    
 	    public void setEntityId(int id)
@@ -27,5 +30,13 @@ public class TileEntityCharredWood extends TileEntity
 	    public int getEntityId()
 	    {
 	    	return entityId;
+	    }
+	    
+	    public void updateEntity() 
+	    {
+	    	if(getEntityId() != 0)
+	    	{
+	    		
+	    	}
 	    }
 }
