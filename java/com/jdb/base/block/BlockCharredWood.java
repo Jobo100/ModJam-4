23f5d51d032608c98d8cf5133149231ef3ca0b1f
Joe <josephdb100@gmail.com>
2014-05-15 17:15:24 -0600

    ok almost done charred wood mechanics

--- java/com/jdb/base/block/BlockCharredWood.java
+++ java/com/jdb/base/block/BlockCharredWood.java
@@ -1,11 +1,21 @@
 package com.jdb.base.block;
 
+import com.jdb.base.item.ItemKnife;
+import com.jdb.base.item.ItemSoulJar;
+
 import net.minecraft.block.Block;
+import net.minecraft.block.ITileEntityProvider;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.world.World;
 
 public class BlockCharredWood extends Block {
 
+	public boolean carved = false;
+	private TileEntityCharredWood tileEntity;
+	
 	protected BlockCharredWood(Material material) 
 	{
 		super(material);
@@ -15,6 +25,28 @@ public class BlockCharredWood extends Block {
 		this.setBlockName("charredWood");
 		this.setHardness(2.0F);
 		this.setResistance(7.0F);
+		this.setTickRandomly(true);
 	}
-
+	
+	@Override
+    public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int what, float are, float these, float questionMark)
+    {
+    	if(player.inventory.getCurrentItem().getItem() instanceof ItemKnife)
+    	{
+    		carved = true;
+    		return true;
+    	}
+    	if(player.inventory.getCurrentItem().getItem() instanceof ItemSoulJar && player.inventory.getCurrentItem().stackTagCompound.getInteger("Entity Id") != 0)
+    	{
+    		tileEntity.setEntityId(player.inventory.getCurrentItem().stackTagCompound.getInteger("Entity Id"));
+    		return true;
+    	}
+        return false;
+    }
+    
+    public TileEntity createTileEntity(World world, int metadata)
+    {
+    	tileEntity = new TileEntityCharredWood();
+    	return tileEntity;
+    }
 }
