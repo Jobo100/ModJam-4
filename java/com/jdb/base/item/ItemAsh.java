114be8f272c030348261ff7963a3296bc5143c0c
Joe <josephdb100@gmail.com>
2014-05-15 01:22:26 -0600

    Finished Ash Pile and Ash Item. Taking a quick break need to eat.

---
+++ java/com/jdb/base/item/ItemAsh.java
@@ -0,0 +1,14 @@
+package com.jdb.base.item;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+
+public class ItemAsh extends Item
+{
+	public ItemAsh()
+	{
+		this.setUnlocalizedName("ash");
+		this.setTextureName("ash");
+		this.setCreativeTab(CreativeTabs.tabMisc);
+	}
+}
